8b1e18af8bdccc6844de68a61369afe9c355f460
Mirkhamfaozan <100754810+Mirkhamfaozan@users.noreply.github.com>
2022-04-18 15:32:09 +0700

    Add files via upload

---
+++ UTS SOAL NO 2.java
@@ -0,0 +1,31 @@
+public class Instagram {
+    int likes;
+    int komen;
+}
+public class komentar {
+    public static void main(String[] args) {
+        
+    }
+
+//method dari komentar
+
+    Instagram = new Instagram
+
+    Instagram.likes ="260";
+    Instagram.komen ="1000";
+
+    System.out.println("260";
+    System.out.println("1000"))
+void icon postingan() {
+    System.out.println("gambar instagram.");
+}
+
+void likes() {
+    System.out.println("jumlah likes.");
+}
+
+    
+}
+
+    
+}
